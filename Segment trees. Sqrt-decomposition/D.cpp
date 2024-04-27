6bba39021415a1de2ebd01b15c95b697fc6ec122
osman <osmanlyumanov2005@gmail.com>
2024-04-27 19:11:44 +0300

    Оптимизирована по скорости задача D. Хотя все еще не проходит

--- Segment trees. Sqrt-decomposition/D.cpp
+++ Segment trees. Sqrt-decomposition/D.cpp
@@ -1,43 +1,55 @@
 #include <iostream>
 using namespace std;
 
-void fillArray(unsigned int A[], unsigned int commands[][3], unsigned int command[])
+void fill_array(unsigned int diff_arr[], unsigned int A[], unsigned int n, unsigned int commands[][3], unsigned int m, unsigned int p1 = 0, unsigned int p2 = 0)
 {
-    if (command[0] == 1)
+    if (p2 == 0)
+        p2 = m;
+
+    for (unsigned int i = p1; i < p2; i++)
     {
-        for (unsigned int i = command[1] - 1; i < command[2]; i++)
-            A[i]++;
-    }
-    else
-    {
-        for (unsigned int i = command[1] - 1; i < command[2]; i++)
-            fillArray(A, commands, commands[i]);
+        if (commands[i][0] == 1)
+        {
+            diff_arr[commands[i][1] - 1] += 1;
+            diff_arr[commands[i][2]] -= 1;
+        }
+        else
+            fill_array(diff_arr, A, n, commands, m, commands[i][1] - 1, commands[i][2]);
     }
 }
 
 int main()
 {
-    unsigned int N, M;
-    cin >> N >> M;
+    unsigned int n, m;
+    cin >> n >> m;
 
-    unsigned int A[N];
-    for (unsigned int i = 0; i < N; i++)
+    unsigned int A[n];
+    for (unsigned int i = 0; i < n; i++)
         A[i] = 0;
 
-    unsigned int commands[M][3];
-    for (unsigned int i = 0; i < M; i++)
+    unsigned int commands[m][3];
+    for (unsigned int i = 0; i < m; i++)
         cin >> commands[i][0] >> commands[i][1] >> commands[i][2];
 
-    for (unsigned int i = 0; i < M; i++)
-        fillArray(A, commands, commands[i]);
+    unsigned int diff_arr[n + 1];
+    for (unsigned int i = 0; i < n + 1; i++)
+        diff_arr[i] = 0;
 
-    unsigned int i = 0;
-    while (i < N - 1)
+    fill_array(diff_arr, A, n, commands, m);
+
+    A[0] = diff_arr[0];
+    for (unsigned int i = 1; i < n + 1; i++)
+        A[i] = A[i - 1] + diff_arr[i];
+
     {
-        cout << A[i] << " ";
-        i++;
+        unsigned int i = 0;
+        while (i < n - 1)
+        {
+            cout << A[i] << " ";
+            i++;
+        }
+        cout << A[i] << endl;
     }
-    cout << A[i] << endl;
 
     return 0;
 }
--- Segment trees. Sqrt-decomposition/D.py
+++ Segment trees. Sqrt-decomposition/D.py
@@ -1,15 +1,43 @@
-def fill_the_array(command):
-    if command[0] == 1:
-        for i in range(command[1] - 1, command[2]):
-            A[i] += 1
-    else:
-        for i in range(command[1] - 1, command[2]):
-            fill_the_array(commands[i])
+def optimized_fill_array(A, commands):
+    # Создаем разностный массив
+    diff = [0] * (len(A) + 1)
+
+    # Применяем команды к разностному массиву
+    for command in commands:
+        if command[0] == 1:
+            # Увеличиваем значение в начальном индексе
+            diff[command[1] - 1] += 1
+            # Уменьшаем значение в конечном индексе + 1, чтобы обнулить эффект после конечного индекса
+            if command[2] < len(A):
+                diff[command[2]] -= 1
+        else:
+            # Для команды типа 2 рекурсивно применяем ссылочные команды
+            for i in range(command[1] - 1, command[2]):
+                optimized_fill_array(A, [commands[i]])
+
+    # Строим исходный массив из разностного массива
+    A[0] = diff[0]
+    for i in range(1, len(A)):
+        A[i] = A[i - 1] + diff[i]
 
 
-N, M = list(map(int, input().split()))
-commands = tuple(tuple(map(int, input().split())) for _ in range(0, M))
-A = [0] * N
-for i in range(0, M):
-    fill_the_array(commands[i])
-print(*A)
+# Пример использования:
+A = [0] * 5  # Предполагаем массив размером 5
+commands = [
+    [1, 1, 2],
+    [1, 4, 5],
+    [2, 1, 2],
+    [2, 1, 3],
+    [2, 3, 4],  # Эта команда ссылается на первые две команды
+]
+optimized_fill_array(A, commands)
+print(A)
+# Пример использования:
+# N, M = list(map(int, input().split()))
+# A = [0] * N
+# commands = []
+# [commands.append(list(map(int, input().split()))) for _ in range(0, M)]
+# print(A)
+# print(commands)
+# optimized_fill_array(A, commands)
+# print(A)
